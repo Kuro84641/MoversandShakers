7e968303c27565f208e2f59554ab970cd7dab9f7
Kuro84641 <samueljgo66@yahoo.com>
2026-02-15 22:41:43 +0000

    making trap

--- Assets/ItemSystem/Trap/activation.cs
+++ Assets/ItemSystem/Trap/activation.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Trap : MonoBehaviour
+public class activation : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
